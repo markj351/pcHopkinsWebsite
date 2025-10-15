588621809a8d06ff5181cec48579e1a08b14a852
Mark <markbob666@gmail.com>
2025-10-14 23:50:26 -0400

    install prompt

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js");
   });
 }
+
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  if (installBtn) installBtn.hidden = false;
+});
+
+installBtn?.addEventListener("click", async () => {
+  installBtn.hidden = true;
+  deferredPrompt?.prompt();
+  await deferredPrompt?.userChoice;
+  deferredPrompt = null;
+});
